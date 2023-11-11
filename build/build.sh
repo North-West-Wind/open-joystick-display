95365f010a9a2d100d81f1ae8a5432827fff9d24
North-West-Wind <wsyn148@gmail.com>
2023-11-11 12:21:00 +0800

    correct name

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,6 @@ set -e
 docker --version
 docker build -t ojd-2856b298-311f-402e-b904-6d93068140f0 .
 docker create --name ojd-cont-2856b298-311f-402e-b904-6d93068140f0 ojd-2856b298-311f-402e-b904-6d93068140f0
-docker cp ojd-cont-2856b298-311f-402e-b904-6d93068140f0:/open-joystick-display/dist .
+docker cp ojd-cont-2856b298-311f-402e-b904-6d93068140f0:/open-joystick-display-transparent/dist .
 docker rm -f ojd-cont-2856b298-311f-402e-b904-6d93068140f0
 docker image rm -f ojd-2856b298-311f-402e-b904-6d93068140f0
